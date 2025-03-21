7da9a7efebe8d676440ff02cc586317a42c3f8c4
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-21 16:20:08 +0100

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Eric on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
